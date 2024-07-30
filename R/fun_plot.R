c18868045669f2d54e02404eb4bbb5a748f0f26c
Benjamin Lang <23262298+lang-benjamin@users.noreply.github.com>
2024-07-30 08:11:21 +0200

    Update fun_plot.R

--- R/fun_plot.R
+++ R/fun_plot.R
@@ -5,7 +5,7 @@ theme_classical <- function(base_family = "", base_size = 12,
   axis_col <- if (borders_gray) "gray70" else "black" # axis line and tick mark color
   axis_text_col <- if (borders_gray) "gray40" else "black" # tick mark labels and labels for axes
   axis_size <- 0.2
-  axis_text_size <- base_size - 1 # tick mark label
+  axis_text_size <- base_size - 2 # tick mark label
   axis_title_size <- base_size # y or x axis title/label
   axis_title_family <- base_family
   strip_text_size <- base_size + 1
