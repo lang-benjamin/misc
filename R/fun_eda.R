80d55321395fa600fb1d58f854f734ac8650ca76
Benjamin Lang <23262298+lang-benjamin@users.noreply.github.com>
2025-09-17 06:28:07 +0100

    update of print_levels

--- R/fun_eda.R
+++ R/fun_eda.R
@@ -1,24 +1,37 @@
-# Provide summary statistics for cleaning EDA
-# Function is taken from John Curtin: https://github.com/jjcurtin/lab_support/blob/main/fun_eda.R
-skim_some <- skimr::skim_with(numeric = skimr::sfl(mean = NULL, sd = NULL, p25 = NULL, p50 = NULL, p75 = NULL, hist = NULL))
-
-# Function is taken from John Curtin: https://github.com/jjcurtin/lab_support/blob/main/fun_eda.R
-print_kbl <- function(data, height = "500px", align = "r", digits = 2, caption = NULL) {
-  data |>
-    kableExtra::kbl(align = align, digits = digits, caption = caption) |>
-    kableExtra::kable_styling(bootstrap_options = c("striped", "condensed")) |>
-    kableExtra::scroll_box(height = height, width = "100%")
-}
-
-# Print levels or unique values of factor / character variables in a data frame
-print_levels <- function(d) {
-  for (x in names(d)) {
+print_levels <- function(d, print_continuous = FALSE, sort_variables = FALSE) {
+  variable_names <- names(d)  # Get all variable names
+  
+  if (sort_variables) {
+    variable_names <- sort(variable_names)
+  }
+  
+  for (i in seq_along(variable_names)) {
+    x <- variable_names[i]
+    
+    # Skip continuous variables entirely if print_continuous = FALSE
+    if ((is.numeric(d[[x]]) || is.integer(d[[x]])) && !print_continuous) {
+      next  # Skip to the next variable
+    }
+    var_label <- Hmisc::label(d[[x]])
+    if (var_label == "")
+      cat(x, ": ", sep = "")
+    else
+      cat(x, " (", var_label, "): ", sep = "")
+    
     if (is.factor(d[[x]])) {
-      cat("Variable:", x, "\n")
-      cat("Levels:", paste(levels(d[[x]]), collapse = "; "), "\n\n")
+      cat(paste(levels(d[[x]]), collapse = "; "), if (is.ordered(d[[x]])) "(ordered factor)" else "(unordered factor)", "\n")
     } else if (is.character(d[[x]])) {
-      cat("Variable:", x, "\n")
-      cat("Unique Values:", paste(unique(d[[x]]), collapse = "; "), "\n\n")
+      cat(paste(unique(d[[x]]), collapse = "; "), "(unique values)", "\n")
+    } else if (is.numeric(d[[x]]) || is.integer(d[[x]])) {
+      if (print_continuous) {
+        lowest_values <- sort(d[[x]])[1:5]
+        highest_values <- sort(d[[x]], decreasing = TRUE)[1:5]
+        cat("lowest: ", paste(lowest_values, collapse = " "),
+            ", highest: ", paste(highest_values, collapse = " "), "\n", sep = "")
+      }
+    }
+    if (i < length(variable_names)) {
+      cat("\n")
     }
   }
 }
